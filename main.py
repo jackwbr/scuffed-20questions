dc888e8632f1d2b95a1bf498ec4dd2cac15175e7
Jack W <jackweber943@gmail.com>
2023-09-09 21:12:09 -0400

    Updates
    Code is unworking currently.

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("test")
+objects = ['potato', 'test', 'object']
+print(f"Here are your options, please choose one: {', '.join(objects)}")
+
+input("Please choose an option: ")
+
+def falseinput():
+    input("Please choose a valid option: ")
+
+if input not in objects:   
+    falseinput() 
+
+def brute():
+    while True:
+        input(f"Is your object {objects[0]}? Please respond with yes or no: ")
+        if input == "yes":
+            print("I knew it!") 
+            break
+
