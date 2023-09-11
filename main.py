85d13c45d9abeb3b4424040bf85732089b304236
Jack W <jackweber943@gmail.com>
2023-09-11 10:28:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
-objects = ['potato', 'test', 'object']
+objects = ["potato", "test", "object"]
 print(f"Here are your options, please choose one: {', '.join(objects)}")
 index = 0
-
-input("Please choose an option: ")
 
 def falseinput():
     input("Please choose a valid option: ")
 
+input("Please choose an option: ")
+
+
 if input not in objects:   
-    falseinput() 
+        falseinput()
 
-def brute():
-    for x in range(20):
-        input(f"Is your object {objects[0]}? Please respond with yes or no: ")
-        if input == "yes":
-            print("I knew it!") 
-            break
 
-        elif input == "no":
-            index += 1
-            input(f"Is your object {objects[index]}? Please respond with yes or no: ")
+
+# def brute():
+#     for x in range(20):
+#         input(f"Is your object {objects[0]}? Please respond with yes or no: ")
+#         if input == "yes":
+#             print("I knew it!") 
+#             break
+
+#         elif input == "no":
+#             index += 1
+#             input(f"Is your object {objects[index]}? Please respond with yes or no: ")
