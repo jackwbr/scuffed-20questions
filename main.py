bac5046f996dd2f74eab0832fa31649b4a1aa6b5
Jack W <jackweber943@gmail.com>
2023-09-11 13:11:46 -0400

    Update main.py
    Fixed!

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+indexalpha = 0
 theobjects = ['potato', 'test', 'yesandno']
 print("These are your options") 
 print(theobjects)
@@ -10,7 +11,16 @@ def userinput():
         else:
             print("Please choose a valid option")
 
-def scuffed():
+def scuffed(indexalpha):
     while True:
+        input5 = input(f"Is what you are thinking of {theobjects[indexalpha]}? Please respond with yes or no:")
+        if input5 == "yes":
+            break
+
+        else:
+            indexalpha += 1
+            continue
+
 
 userinput()
+scuffed(indexalpha)
