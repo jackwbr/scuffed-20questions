bace4db11b0293d79378a5379bae90eccb679ad5
Jack W <jackweber943@gmail.com>
2023-09-11 13:05:57 -0400

    Update main.py
    Fixed bug but deleted most code.

--- main.py
+++ main.py
@@ -1,26 +1,16 @@
-objects = ["potato", "test", "object"]
-print(f"Here are your options, please choose one: {', '.join(objects)}")
-indexalpha = 0
+theobjects = ['potato', 'test', 'yesandno']
+print("These are your options") 
+print(theobjects)
 
 def userinput():
     while True:
-        input0 = input("Please choose an option: ")     
-        if input0 in objects:          #if the users input is valid e.g. in the list, the loop breaks
+        userinput1 = input("Please choose an option: ")
+        if userinput1 in theobjects:
             break
-        else:                          #if the users input is not valid e.g. in the list, the loop conintues
-            continue
+        else:
+            print("Please choose a valid option")
 
-print(userinput)
+def scuffed():
+    while True:
 
-# def brute():
-#     for x in range(20):
-#         input(f"Is your object {objects[0]}? Please respond with yes or no: ")
-#         if input == "yes":
-#             print("I knew it!") 
-#             break
-
-#         elif input == "no":
-#             indexalpha += 1
-#             input(f"Is your object {objects[indexalpha]}? Please respond with yes or no: ")
-
-userinput()                             # This calls the userinput function to get the users input
+userinput()
