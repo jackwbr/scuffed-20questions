c61d33f183fbde2c466c725e553da365df198cb4
Jack W <jackweber943@gmail.com>
2023-09-19 12:54:01 -0400

    Update main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+def difficulty():
+        while True:
+            difficultylevel = input("Please select a difficulty level: easy or hard: ")
+            if difficultylevel == "easy":
+             break
+            else:
+                continue
+
+
+
+#END OF FUNCTION DEFINITIONS
+
+difficulty()
