32751b65726d9e6d19abb50a116e9d930ed5611c
Jack W <jackweber943@gmail.com>
2023-09-20 12:50:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ def difficultyselect(userinput):
         userinput = userinput.lower().strip()
         if userinput == "easy" or "hard":
          return userinput
+        
         else: 
             print("Please choose a valid input: ")
             continue
 
 difficultyselect(userinput)
+print(userinput)
