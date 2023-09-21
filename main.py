3b4d0cfe39f1d8b3f735f6e5ce06d5f5e41e5ec6
Jack W <jackweber943@gmail.com>
2023-09-21 08:43:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 def difficultyselect():
     while True:
+        global userinput
         userinput = input("Please select a difficulty, easy or hard: ")
         if userinput in ("easy", "hard"):
-            print("good")
             break
         else:
             print("Please enter a valid difficulty")
             continue
 
+def easy():
+    if userinput == "easy":
+        options = ["potato", "test", "cheese", "spam", "eggs"]
+        print(*options)
+        while True:
+            userinput = input("Please enter an option from the list above: ")
+            if userinput in options:
+                break
+            else:
+                print("Please enter a valid option")
+                continue        
+
+
+#END OF FUNCTION DEFINITIONS
+
 difficultyselect()
+if userinput == "easy":
+    easy()
