5d083f122263e590268a38863b56a7d604ca611f
Jack W <jackweber943@gmail.com>
2023-09-23 13:44:25 -0400

    Update main.py
    Play again feature is in progress

--- main.py
+++ main.py
@@ -1,63 +1,79 @@
 import time
 options = ["potato", "test", "cheese", "spam", "eggs"]
-global userinput
-
-def difficultyselect():
+while True:
     global userinput
-    while True:
-        userinput = input("Please select a difficulty, easy or hard: ")
-        userinput = userinput.lower().strip()
-        if userinput in ("easy", "hard"):
-            break
-        else:
-            print("Please enter a valid difficulty")
-            continue
 
-def easy():
-        global easydoinput
-        print(*options, sep = ", ")
+    def difficultyselect():
+        global userinput
         while True:
-            easydoinput = input("Please enter an option from the list above: ")
-            easydoinput = easydoinput.lower().strip()
-            if easydoinput in options:
+            userinput = input("Please select a difficulty, easy or hard: ")
+            userinput = userinput.lower().strip()
+            if userinput in ("easy", "hard"):
                 break
-            else:     
-                print("Please enter a valid option")
-                continue        
+            else:
+                print("Please enter a valid difficulty")
+                continue
 
-def easydo():
-    easyanswer = input(f"Is {easydoinput} what you were thinking of? ")
-    easyanswer = easyanswer.lower().strip()
-    if easyanswer == "yes":
-        print("Knew it!")
-    else:
-        print("What the hell?")
+    def easy():
+            global easydoinput
+            print(*options, sep = ", ")
+            while True:
+                easydoinput = input("Please enter an option from the list above: ")
+                easydoinput = easydoinput.lower().strip()
+                if easydoinput in options:
+                    break
+                else:     
+                    print("Please enter a valid option")
+                    continue        
 
-def harddo():
-    global indexalpha
-    indexalpha = 0
-    print(*options, sep = ", ")
-    print("Please think of an option from the list above")
-    time.sleep(4)
-    while True:
-        print(f"Is your option {options[indexalpha]}?")   
-        isyours = input("Please enter yes or no: ")
-        isyours = isyours.lower().strip()
-        if isyours == "yes":
-            print("Knew it! Good game.")                    #ADD PLAY AGAIN FEATURE
-            break
-        elif isyours == "no":
-            indexalpha = indexalpha + 1
-            continue
+    def easydo():
+        easyanswer = input(f"Is {easydoinput} what you were thinking of? ")
+        easyanswer = easyanswer.lower().strip()
+        if easyanswer == "yes":
+            endgame = input("Knew it! Do you want to play again? ")  
+            endgame = endgame.strip().lower()    
+            if endgame == "yes":
+                print("Alright! Let's go again!")
+                continue
+            if endgame == "no":
+                print("Alright! Thanks for playing!")
+                exit()            #ADD PLAY AGAIN FEATURE          
+        else:
+            print("What the hell?")
+
+    def harddo():
+        global indexalpha
+        indexalpha = 0
+        print(*options, sep = ", ")
+        print("Please think of an option from the list above")
+        time.sleep(4)
+        while True:
+            print(f"Is your option {options[indexalpha]}?")   
+            isyours = input("Please enter yes or no: ")
+            isyours = isyours.lower().strip()
+            if isyours == "yes":
+                endgame = input("Knew it! Good game. Do you want to play again? ")   
+                endgame = endgame.strip().lower() 
+                if endgame == "yes":
+                    print("Alright! Let's go again!")
+                    time.sleep(1.5)
+                    continue
+                if endgame == "no":
+                    print("Ok, thanks for playing!")
+                    exit()                #ADD PLAY AGAIN FEATURE
+                break
+            elif isyours == "no":
+                indexalpha = indexalpha + 1
+                continue
 
 
-#END OF FUNCTION DEFINITIONS
+    #END OF FUNCTION DEFINITIONS
 
-difficultyselect()
-if userinput == "easy":
-    easy()
-    easydo()
+    difficultyselect()
+    if userinput == "easy":
+        easy()
+        easydo()
 
 
-if userinput == "hard":
-    harddo()
+    if userinput == "hard":
+        harddo()
