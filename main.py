ad79c5294ec84dfe8439dd2823849fcc953a0187
Jack W <jackweber943@gmail.com>
2023-09-23 13:35:34 -0400

    Working now!

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import time
-global indexalpha
-indexalpha = 0
 options = ["potato", "test", "cheese", "spam", "eggs"]
 global userinput
 
@@ -8,6 +6,7 @@ def difficultyselect():
     global userinput
     while True:
         userinput = input("Please select a difficulty, easy or hard: ")
+        userinput = userinput.lower().strip()
         if userinput in ("easy", "hard"):
             break
         else:
@@ -19,6 +18,7 @@ def easy():
         print(*options, sep = ", ")
         while True:
             easydoinput = input("Please enter an option from the list above: ")
+            easydoinput = easydoinput.lower().strip()
             if easydoinput in options:
                 break
             else:     
@@ -27,18 +27,22 @@ def easy():
 
 def easydo():
     easyanswer = input(f"Is {easydoinput} what you were thinking of? ")
+    easyanswer = easyanswer.lower().strip()
     if easyanswer == "yes":
         print("Knew it!")
     else:
         print("What the hell?")
 
 def harddo():
+    global indexalpha
+    indexalpha = 0
     print(*options, sep = ", ")
     print("Please think of an option from the list above")
     time.sleep(4)
     while True:
         print(f"Is your option {options[indexalpha]}?")   
         isyours = input("Please enter yes or no: ")
+        isyours = isyours.lower().strip()
         if isyours == "yes":
             print("Knew it! Good game.")                    #ADD PLAY AGAIN FEATURE
             break
